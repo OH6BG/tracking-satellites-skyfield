df65f9d8b4cf517311a7180c9ebc7dff7e0ed77e
Jari Perkiömäki <OH6BG@users.noreply.github.com>
2018-12-18 20:36:44 +0200

    The MIT License

--- tracking_satellites.py
+++ tracking_satellites.py
@@ -1,10 +1,29 @@
+'''
+Copyright 2018 Jari Perkiömäki OH6BG.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
 from skyfield.api import Topos, load
 
 ts = load.timescale()
 t = ts.now()
 
 # Observer coordinates
-lat, lon = 63.146, 21.542
+lat, lon = 40.7128, -74.0060
 # Observer date
 year, month, day = 2018, 12, 18
 
